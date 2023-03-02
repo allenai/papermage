b66cab93d0a69f336d2afb6612544816172a7c7d
kyleclo <kyleclo@uw.edu>
2023-03-02 02:54:17 -0800

    add Annotation base class

--- papermage/types/__init__.py
+++ papermage/types/__init__.py
@@ -2,9 +2,12 @@
 from papermage.types.span import Span
 from papermage.types.box import Box
 from papermage.types.metadata import Metadata
+from papermage.types.annotation import Annotation
+from papermage.types.entity import Entity
 
 __all__ = [
     'Document',
+    'Annotation'
     'Entity',
     'Relation',
     'Span',
--- papermage/types/annotation.py
+++ papermage/types/annotation.py
@@ -11,21 +11,14 @@ Document object directly from within the Entity itself.
 import logging
 
 from abc import abstractmethod
-from typing import TYPE_CHECKING, Dict, List, Optional, Union
-
-from papermage.types import Span
-from papermage.types import Box
-from papermage.types.metadata import Metadata
+from typing import TYPE_CHECKING, Dict, List, Union, Optional
 
 if TYPE_CHECKING:
     from mmda.types.document import Document
 
-__all__ = ["Annotation", "Entity", "Relation"]
-
-
 
 class Annotation:
-    """Annotation allows us to layer different model predictions on a single document."""
+    """Represent a "unit" (e.g. highlighted span, drawn boxes) layered on a Document."""
 
     @abstractmethod
     def __init__(self):
@@ -41,11 +34,13 @@ class Annotation:
         return self._doc
 
     @doc.setter
-    def doc(self, doc: Document) -> None:
+    def doc(self, doc: 'Document') -> None:
         """This method attaches a Document to this Annotation, allowing the Annotation
-                to access things beyond itself within the Document (e.g. neighbors)"""
+        to access things beyond itself within the Document (e.g. neighboring annotations)"""
         if self.doc:
-            raise AttributeError("This annotation already has an attached document")
+            raise AttributeError("Already has an attached Document. Since Annotations should be"
+                                 "specific to a given Document, we recommend creating a new"
+                                 "Annotation from scratch and then attaching your Document.")
         self._doc = doc
 
     @property
@@ -58,117 +53,16 @@ class Annotation:
         to this Annotation. ID basically gives the Annotation itself awareness of its
         position within the broader Document."""
         if self.id:
-            raise AttributeError("This annotation already has an ID")
+            raise AttributeError(f"This Annotation already has an ID: {self.id}")
         if not self.doc:
-            raise AttributeError('This annotation is missing a Document')
+            raise AttributeError('This Annotation is missing a Document')
         self._id = id
 
     @abstractmethod
-    def to_json(self) -> Dict:
+    def to_json(self) -> Union[Dict, List]:
         pass
 
     @classmethod
     @abstractmethod
-    def from_json(cls, annotation_dict: Dict) -> "Annotation":
+    def from_json(cls, annotation_json: Union[Dict, List]) -> "Annotation":
         pass
-
-    def __getattr__(self, field: str) -> List["Annotation"]:
-        """This method """
-        if self.doc is None:
-            raise ValueError("This annotation is not attached to a document")
-
-        if field in self.doc.fields:
-            return self.doc.find_overlapping(self, field)
-
-        if field in self.doc.fields:
-            return self.doc.find_overlapping(self, field)
-
-        return self.__getattribute__(field)
-
-
-class Entity(Annotation):
-    def __init__(
-            self,
-            spans: List[Span],
-            boxes: Optional[List[Box]] = None,
-            metadata: Optional[Metadata] = None
-    ):
-        self.spans = spans
-        self.boxes = boxes
-        self.metadata = metadata if metadata else Metadata
-        super().__init__()
-
-    @property
-    def symbols(self) -> List[str]:
-        if self.doc is None:
-            raise ValueError(f'No document attached.')
-        return [self.doc.symbols[span.start: span.end] for span in self.spans]
-
-    @property
-    def text(self) -> str:
-        maybe_text = self.metadata.get("text", None)
-        if maybe_text is None:
-            return " ".join(self.symbols)
-        return maybe_text
-
-    @text.setter
-    def text(self, text: Union[str, None]) -> None:
-        self.metadata.text = text
-
-    def to_json(self) -> Dict:
-        entity_dict = dict(
-            spans=[span.to_json() for span in self.spans],
-            boxes=[box.to_json() for box in self.boxes] if self.boxes else None,
-            metadata=self.metadata.to_json()
-        )
-        # only serialize non-null values
-        return {k: v for k, v in entity_dict.items() if v is not None}
-
-    @classmethod
-    def from_json(cls, entity_dict: Dict) -> "Entity":
-        return cls(
-            spans=[Span.from_json(span_json=span_dict) for span_dict in entity_dict["spans"]],
-            boxes=[Box.from_json(box_dict=box_dict) for box_dict in entity_dict['boxes']]
-            if entity_dict.get('boxes') else None,
-            metadata=Metadata.from_json(entity_dict['metadata'])
-            if entity_dict.get('metadata') else None
-        )
-
-    @property
-    def start(self) -> int:
-        return min([span.start for span in self.spans])
-
-    @property
-    def end(self) -> int:
-        return max([span.end for span in self.spans])
-
-
-class Relation(Annotation):
-    def __init__(
-            self,
-            source: Entity,
-            target: Entity,
-            metadata: Optional[Metadata] = None
-    ):
-        self.source = source
-        self.target = target
-        self.metadata = metadata if metadata else Metadata
-        super().__init__()
-
-    def to_json(self) -> Dict:
-        relation_dict = dict(
-            source=Entity.id,
-            target=Entity.id,
-            metadata=self.metadata.to_json()
-        )
-        # only serialize non-null values
-        return {k: v for k, v in relation_dict.items() if v is not None}
-
-    @classmethod
-    def from_json(cls, relation_dict: Dict, doc: Document) -> "Relation":
-        return cls(
-            source=None,
-            target=None,
-            metadata=Metadata.from_json(relation_dict['metadata'])
-            if relation_dict.get('metadata') else None
-        )
