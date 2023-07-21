bcbe1982be01cb885a4f2e7404a2b60fcfc3d5a2
kyleclo <kyleclo@uw.edu>
2023-07-20 21:55:16 -0700

    fix rasterizer test

--- tests/test_rasterizers/test_pdf2image_rasterizer.py
+++ tests/test_rasterizers/test_pdf2image_rasterizer.py
@@ -15,11 +15,10 @@ from papermage.types import Image
 class TestPDF2ImageRasterizer(unittest.TestCase):
     def setUp(cls) -> None:
         cls.fixture_path = pathlib.Path(__file__).parent.parent / "fixtures"
-    
+
     def test_raseterize(self):
         rasterizer = PDF2ImageRasterizer()
-        images = rasterizer.rasterize(input_pdf_path=self.fixture_path / "1903.10676.pdf", dpi=72)
-        assert len(images) == 1
-        assert images[0].pilimage.size == (595, 842)
+        images = rasterizer.rasterize(input_pdf_path=self.fixture_path / "2304.02623v1.pdf", dpi=72)
+        assert len(images) == 4
+        assert images[0].pilimage.size == (612, 792)
         assert isinstance(images[0], Image)
-        
