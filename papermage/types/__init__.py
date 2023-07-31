a5b0061de6e3555651f2a3f5821d9f75824c8a9d
kyleclo <kyleclo@uw.edu>
2023-07-31 06:26:24 -0700

    add test for doc annotate

--- papermage/types/__init__.py
+++ papermage/types/__init__.py
@@ -1,3 +1,11 @@
+"""
+
+Needs to be imported in this order.
+
+@kylel
+
+"""
+
 from papermage.types.image import Image
 from papermage.types.span import Span
 from papermage.types.box import Box
@@ -5,9 +13,17 @@ from papermage.types.metadata import Metadata
 from papermage.types.annotation import Annotation
 from papermage.types.entity import Entity
 from papermage.types.indexer import EntitySpanIndexer
-from papermage.types.document import Document, \
-    MetadataFieldName, EntitiesFieldName, SymbolsFieldName, RelationsFieldName
-
+from papermage.types.document import Document
+from papermage.types.document import (
+    MetadataFieldName, 
+    EntitiesFieldName, 
+    SymbolsFieldName,
+    RelationsFieldName, 
+    PagesFieldName, 
+    TokensFieldName, 
+    RowsFieldName
+)
+    
 
 __all__ = [
     'Document',
@@ -22,5 +38,8 @@ __all__ = [
     'SymbolsFieldName',
     'MetadataFieldName',
     'EntitiesFieldName',
-    'RelationsFieldName'
+    'RelationsFieldName',
+    'PagesFieldName', 
+    'TokensFieldName', 
+    'RowsFieldName'
 ]
