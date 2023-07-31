c457bce1d140e648d0ba5b24f16dd6915b67377a
kyleclo <kyleclo@uw.edu>
2023-07-31 04:35:33 -0700

    black formatting

--- papermage/predictors/entity_classification_predictor.py
+++ papermage/predictors/entity_classification_predictor.py
@@ -4,40 +4,41 @@
 
 """
 
-from typing import Dict, List, Any, Tuple, Optional, Sequence
-
 from collections import defaultdict
-import numpy as np
+from typing import Any, Dict, List, Optional, Sequence, Tuple
 
+import numpy as np
 import torch
 import transformers
 from smashed.interfaces.simple import (
-    TokenizerMapper,
-    UnpackingMapper,
     FixedBatchSizeMapper,
     FromTokenizerListCollatorMapper,
     Python2TorchMapper,
+    TokenizerMapper,
+    UnpackingMapper,
 )
 
+from papermage.predictors.base_predictor import BasePredictor
 from papermage.types import Annotation, Document, Entity, Metadata, Span
-from papermage.predictors.hf_predictors.base_hf_predictor import BaseHFPredictor
 
 
 class EntityClassificationBatch:
     def __init__(
-            self,
-            input_ids: List[List[int]],
-            attention_mask: List[List[int]],
-            entity_ids: List[List[Optional[int]]],
-            context_id: List[int]
+        self,
+        input_ids: List[List[int]],
+        attention_mask: List[List[int]],
+        entity_ids: List[List[Optional[int]]],
+        context_id: List[int],
     ):
         if not (len(input_ids) == len(attention_mask) == len(entity_ids) == len(context_id)):
             raise ValueError("Inputs to batch arent same length")
 
         self.batch_size = len(input_ids)
-        if not ([len(example) for example in input_ids] == \
-               [len(example) for example in attention_mask] == \
-               [len(example) for example in entity_ids]):
+        if not (
+            [len(example) for example in input_ids]
+            == [len(example) for example in attention_mask]
+            == [len(example) for example in entity_ids]
+        ):
             raise ValueError("Examples in batch arent same length")
 
         self.input_ids = input_ids
@@ -54,7 +55,7 @@ class EntityClassificationPrediction:
         self.score = score
 
 
-class EntityClassificationPredictor(BaseHFPredictor):
+class EntityClassificationPredictor(BasePredictor):
     """
     This is a generic wrapper around Huggingface Token Classification models.
 
@@ -79,26 +80,27 @@ class EntityClassificationPredictor(BaseHFPredictor):
     REQUIRED_BACKENDS = ["transformers", "torch", "smashed"]
     REQUIRED_DOCUMENT_FIELDS: List[str] = []
 
-    _INPUT_FIELD_NAME = 'inputs'
-    _CONTEXT_ID = 'context_id'
+    _INPUT_FIELD_NAME = "inputs"
+    _CONTEXT_ID = "context_id"
 
-    _HF_RESERVED_INPUT_IDS = 'input_ids'
-    _HF_RESERVED_ATTN_MASK = 'attention_mask'
-    _HF_RESERVED_WORD_IDS = 'word_ids'
+    _HF_RESERVED_INPUT_IDS = "input_ids"
+    _HF_RESERVED_ATTN_MASK = "attention_mask"
+    _HF_RESERVED_WORD_IDS = "word_ids"
     _HF_RESERVED_WORD_PAD_VALUE = -1
 
     def __init__(
-            self,
-            model: Any,
-            config: Any,
-            tokenizer: Any,
-            entity_name: str,
-            context_name: str,
-            batch_size: Optional[int] = 2,
-            device: Optional[str] = 'cpu'
+        self,
+        model: Any,
+        config: Any,
+        tokenizer: Any,
+        entity_name: str,
+        context_name: str,
+        batch_size: Optional[int] = 2,
+        device: Optional[str] = "cpu",
     ):
-        super().__init__(model=model, config=config, tokenizer=tokenizer)
-
+        self.model = model
+        self.config = config
+        self.tokenizer = tokenizer
         self.entity_name = entity_name
         self.context_name = context_name
         self.batch_size = batch_size
@@ -124,7 +126,7 @@ class EntityClassificationPredictor(BaseHFPredictor):
             truncation=True,
             max_length=max_length,
             return_overflowing_tokens=True,
-            return_word_ids=True
+            return_word_ids=True,
         )
         # since input data is automatically chunked into segments (e.g. 512 length),
         # each example <dict> actually becomes many input sequences.
@@ -135,18 +137,15 @@ class EntityClassificationPredictor(BaseHFPredictor):
             fields_to_unpack=[
                 self._HF_RESERVED_INPUT_IDS,
                 self._HF_RESERVED_ATTN_MASK,
-                self._HF_RESERVED_WORD_IDS
+                self._HF_RESERVED_WORD_IDS,
             ],
-            ignored_behavior='repeat'
+            ignored_behavior="repeat",
         )
         # at the end of this, each <dict> contains <lists> of length `batch_size`
         # where each element is variable length within the `max_length` limit.
         # `keep_last` controls whether we want partial batches, which we always do
         # for token classification (i.e. we dont want to miss any token!)
-        self.batch_size_mapper = FixedBatchSizeMapper(
-            batch_size=batch_size,
-            keep_last=True
-        )
+        self.batch_size_mapper = FixedBatchSizeMapper(batch_size=batch_size, keep_last=True)
         # this performs padding so all sequences in a batch are of same length
         self.list_collator_mapper = FromTokenizerListCollatorMapper(
             tokenizer=tokenizer,
@@ -154,27 +153,23 @@ class EntityClassificationPredictor(BaseHFPredictor):
             fields_pad_ids={
                 self._HF_RESERVED_WORD_IDS: self._HF_RESERVED_WORD_PAD_VALUE,
                 "labels": -100,
-            }
+            },
         )
         # this casts python Dict[List] into tensors.  if using GPU, would do `device='gpu'`
-        self.python_to_torch_mapper = Python2TorchMapper(
-            device=device
-        )
+        self.python_to_torch_mapper = Python2TorchMapper(device=device)
         # combining everything
-        self.preprocess_mapper = self.tokenizer_mapper >> \
-                                 self.unpacking_mapper >> \
-                                 self.batch_size_mapper
+        self.preprocess_mapper = self.tokenizer_mapper >> self.unpacking_mapper >> self.batch_size_mapper
 
     @classmethod
     def from_pretrained(
-            cls,
-            model_name_or_path: str,
-            entity_name: str,
-            context_name: str,
-            batch_size: Optional[int] = 2,
-            device: Optional[str] = 'cpu',
-            *args,
-            **kwargs
+        cls,
+        model_name_or_path: str,
+        entity_name: str,
+        context_name: str,
+        batch_size: Optional[int] = 2,
+        device: Optional[str] = "cpu",
+        *args,
+        **kwargs,
     ):
         """If `model_name_or_path` is a path, should be a directory
         containing `vocab.txt`, `config.json`, and `pytorch_model.bin`
@@ -195,23 +190,25 @@ class EntityClassificationPredictor(BaseHFPredictor):
         model = transformers.AutoModelForTokenClassification.from_pretrained(
             pretrained_model_name_or_path=model_name_or_path, *args, **kwargs
         )
-        predictor = cls(model=model, config=config, tokenizer=tokenizer,
-                        entity_name=entity_name, context_name=context_name,
-                        batch_size=batch_size, device=device)
+        predictor = cls(
+            model=model,
+            config=config,
+            tokenizer=tokenizer,
+            entity_name=entity_name,
+            context_name=context_name,
+            batch_size=batch_size,
+            device=device,
+        )
         return predictor
 
-    def preprocess(
-            self,
-            document: Document,
-            context_name: str
-    ) -> List[EntityClassificationBatch]:
+    def preprocess(self, document: Document, context_name: str) -> List[EntityClassificationBatch]:
         """Processes document into whatever makes sense for the Huggingface model"""
         # (1) get it into a dictionary format that Smashed expects
 
         dataset = [
             {
                 self._INPUT_FIELD_NAME: [entity.text for entity in getattr(context, self.entity_name)],
-                self._CONTEXT_ID: i
+                self._CONTEXT_ID: i,
             }
             for i, context in enumerate(getattr(document, context_name))
         ]
@@ -227,15 +224,13 @@ class EntityClassificationPredictor(BaseHFPredictor):
                 input_ids=batch_dict[self._HF_RESERVED_INPUT_IDS],
                 attention_mask=batch_dict[self._HF_RESERVED_ATTN_MASK],
                 entity_ids=batch_dict[self._HF_RESERVED_WORD_IDS],
-                context_id=batch_dict[self._CONTEXT_ID]
-            ) for batch_dict in batch_dicts
+                context_id=batch_dict[self._CONTEXT_ID],
+            )
+            for batch_dict in batch_dicts
         ]
 
     def postprocess(
-            self,
-            doc: Document,
-            context_name: str,
-            preds: List[EntityClassificationPrediction]
+        self, doc: Document, context_name: str, preds: List[EntityClassificationPrediction]
     ) -> List[Annotation]:
         """This function handles a bunch of nonsense that happens with Huggingface models &
         how we processed the data.  Namely:
@@ -291,9 +286,9 @@ class EntityClassificationPredictor(BaseHFPredictor):
         return annotations
 
     def _predict_batch(
-            self,
-            batch: EntityClassificationBatch,
-            device: str = "cpu",
+        self,
+        batch: EntityClassificationBatch,
+        device: str = "cpu",
     ) -> List[EntityClassificationPrediction]:
         #
         #   preprocessing!!  (padding & tensorification)
@@ -302,7 +297,7 @@ class EntityClassificationPredictor(BaseHFPredictor):
             data=self.list_collator_mapper.transform(
                 data={
                     self._HF_RESERVED_INPUT_IDS: batch.input_ids,
-                    self._HF_RESERVED_ATTN_MASK: batch.attention_mask
+                    self._HF_RESERVED_ATTN_MASK: batch.attention_mask,
                 }
             )
         )
@@ -316,25 +311,18 @@ class EntityClassificationPredictor(BaseHFPredictor):
             pytorch_output = self.model(**pytorch_batch)
         scores_tensor = torch.softmax(pytorch_output.logits, dim=2)
         token_scoresss = [
-            [
-                token_scores for token_scores, yn in zip(token_scoress, yns)
-                if yn == 1
-            ]
+            [token_scores for token_scores, yn in zip(token_scoress, yns) if yn == 1]
             for token_scoress, yns in zip(scores_tensor.tolist(), batch.attention_mask)
         ]
         #
         #   postprocessing (map back to original inputs)!!
         #
         preds = []
-        for j, (context_id, word_ids, token_scoress) in enumerate(zip(
-                batch.context_id,
-                batch.entity_ids,
-                token_scoresss)
+        for j, (context_id, word_ids, token_scoress) in enumerate(
+            zip(batch.context_id, batch.entity_ids, token_scoresss)
         ):
             for word_id, token_scores, is_valid_pred in zip(
-                    word_ids,
-                    token_scoress,
-                    self._token_pooling_strategy_mask(word_ids=word_ids)
+                word_ids, token_scoress, self._token_pooling_strategy_mask(word_ids=word_ids)
             ):
                 if word_id is None or is_valid_pred is False:
                     continue
@@ -344,17 +332,17 @@ class EntityClassificationPredictor(BaseHFPredictor):
                         context_id=context_id,
                         entity_id=word_id,
                         label=self.config.id2label[label_id],
-                        score=token_scores[label_id]
+                        score=token_scores[label_id],
                     )
                     preds.append(pred)
         return preds
 
     def _token_pooling_strategy_mask(
-            self,
-            token_ids: Optional[List[int]] = None,
-            word_ids: Optional[List[Optional[int]]] = None,
-            token_scores: Optional[List[Tuple[float, float]]] = None,
-            strategy: str = 'first'
+        self,
+        token_ids: Optional[List[int]] = None,
+        word_ids: Optional[List[Optional[int]]] = None,
+        token_scores: Optional[List[Tuple[float, float]]] = None,
+        strategy: str = "first",
     ) -> List[bool]:
         """
         words are split into multiple tokens, each of which has a prediction.
@@ -366,7 +354,7 @@ class EntityClassificationPredictor(BaseHFPredictor):
         if word_ids is None:
             word_ids = []
 
-        if strategy == 'first':
+        if strategy == "first":
             mask = [True]
             prev_word_id = word_ids[0]
             for current_word_id in word_ids[1:]:
@@ -379,8 +367,5 @@ class EntityClassificationPredictor(BaseHFPredictor):
             raise NotImplementedError(f"mode {strategy} not implemented yet")
 
         # if no word ID (e.g. [cls], [sep]), always mask
-        mask = [
-            is_word if word_id is not None else False
-            for is_word, word_id in zip(mask, word_ids)
-        ]
+        mask = [is_word if word_id is not None else False for is_word, word_id in zip(mask, word_ids)]
         return mask
--- tests/test_predictors/test_entity_classification_predictor.py
+++ tests/test_predictors/test_entity_classification_predictor.py
@@ -4,18 +4,18 @@
 
 """
 
+import json
 import unittest
 
-import json
-
-from papermage.types import Document, Entity, Span
 from papermage.parsers import PDFPlumberParser
-from papermage.predictors.hf_predictors.entity_classification_predictor import (
-    EntityClassificationPredictor
+from papermage.predictors.entity_classification_predictor import (
+    EntityClassificationPredictor,
 )
+from papermage.types import Document, Entity, Span
 
-TEST_SCIBERT_WEIGHTS = 'allenai/scibert_scivocab_uncased'
+TEST_SCIBERT_WEIGHTS = "allenai/scibert_scivocab_uncased"
 # TEST_BIB_PARSER_WEIGHTS = '/Users/kylel/ai2/mmda/stefans/'
+
 
 class TestEntityClassificationPredictor(unittest.TestCase):
     def setUp(self):
@@ -27,19 +27,14 @@ class TestEntityClassificationPredictor(unittest.TestCase):
         self.doc.annotate_entity(field_name="bibs", entities=[ent1, ent2])
         ent1.id = 0
         ent2.id = 1
-        
 
         self.predictor = EntityClassificationPredictor.from_pretrained(
-            model_name_or_path=TEST_SCIBERT_WEIGHTS,
-            entity_name='tokens',
-            context_name='pages'
+            model_name_or_path=TEST_SCIBERT_WEIGHTS, entity_name="tokens", context_name="pages"
         )
 
     def test_predict_pages_tokens(self):
         predictor = EntityClassificationPredictor.from_pretrained(
-            model_name_or_path=TEST_SCIBERT_WEIGHTS,
-            entity_name='tokens',
-            context_name='pages'
+            model_name_or_path=TEST_SCIBERT_WEIGHTS, entity_name="tokens", context_name="pages"
         )
         token_tags = predictor.predict(document=self.doc)
         assert len(token_tags) == len([token for page in self.doc.pages for token in page.tokens])
@@ -50,29 +45,29 @@ class TestEntityClassificationPredictor(unittest.TestCase):
             assert isinstance(token_tag.metadata.score, float)
 
     def test_predict_bibs_tokens(self):
-        self.predictor.context_name = 'bibs'
+        self.predictor.context_name = "bibs"
         token_tags = self.predictor.predict(document=self.doc)
         assert len(token_tags) == len([token for bib in self.doc.bibs for token in bib.tokens])
 
     def test_missing_fields(self):
-        self.predictor.entity_name = 'OHNO'
+        self.predictor.entity_name = "OHNO"
         with self.assertRaises(AssertionError) as e:
             self.predictor.predict(document=self.doc)
-            assert 'OHNO' in e.exception
+            assert "OHNO" in e.exception
 
-        self.predictor.entity_name = 'tokens'
-        self.predictor.context_name = 'BLABLA'
+        self.predictor.entity_name = "tokens"
+        self.predictor.context_name = "BLABLA"
         with self.assertRaises(AssertionError) as e:
             self.predictor.predict(document=self.doc)
-            assert 'BLABLA' in e.exception
+            assert "BLABLA" in e.exception
 
-        self.predictor.context_name = 'pages'
+        self.predictor.context_name = "pages"
 
     def test_predict_pages_tokens_roberta(self):
         predictor = EntityClassificationPredictor.from_pretrained(
             model_name_or_path="roberta-base",
-            entity_name='tokens',
-            context_name='pages',
+            entity_name="tokens",
+            context_name="pages",
             add_prefix_space=True,  # Needed for roberta
         )
         token_tags = predictor.predict(document=self.doc)
