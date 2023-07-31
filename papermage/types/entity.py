dda0de37b01d36bd535620ab4f3c7c109248eb49
kyleclo <kyleclo@uw.edu>
2023-07-31 06:25:27 -0700

    add images to entity

--- papermage/types/entity.py
+++ papermage/types/entity.py
@@ -6,7 +6,7 @@ An annotated "unit" on a Document.
 
 from typing import TYPE_CHECKING, Dict, List, Optional, Union
 
-from papermage.types import Span, Box, Metadata, Annotation
+from papermage.types import Annotation, Box, Image, Metadata, Span
 
 if TYPE_CHECKING:
     from papermage.types.document import Document
@@ -14,29 +14,31 @@ if TYPE_CHECKING:
 
 class Entity(Annotation):
     def __init__(
-            self,
-            spans: Optional[List[Span]] = None,
-            boxes: Optional[List[Box]] = None,
-            metadata: Optional[Metadata] = None
+        self,
+        spans: Optional[List[Span]] = None,
+        boxes: Optional[List[Box]] = None,
+        images: Optional[List[Image]] = None,
+        metadata: Optional[Metadata] = None,
     ):
         if not spans and not boxes:
-            raise ValueError(f'At least one of `spans` or `boxes` must be set.')
+            raise ValueError(f"At least one of `spans` or `boxes` must be set.")
         self.spans = spans if spans else []
         self.boxes = boxes if boxes else []
+        self.images = images if images else []
         self.metadata = metadata if metadata else Metadata()
         super().__init__()
 
     def __repr__(self):
         if self.doc and self.spans:
-            symbols: List[str] = [self.doc.symbols[span.start: span.end] for span in self.spans]
-            return f'Entity\tSymbols\t{symbols}'
-        return f'Entity{self.to_json()}'
+            symbols: List[str] = [self.doc.symbols[span.start : span.end] for span in self.spans]
+            return f"Entity\tSymbols\t{symbols}"
+        return f"Entity{self.to_json()}"
 
     def to_json(self) -> Dict:
         entity_dict = dict(
             spans=[span.to_json() for span in self.spans],
             boxes=[box.to_json() for box in self.boxes],
-            metadata=self.metadata.to_json()
+            metadata=self.metadata.to_json(),
         )
         # only serialize non-null/non-empty values
         return {k: v for k, v in entity_dict.items() if v}
@@ -44,35 +46,23 @@ class Entity(Annotation):
     @classmethod
     def from_json(cls, entity_json: Dict) -> "Entity":
         return cls(
-            spans=[Span.from_json(span_json=span_json)
-                   for span_json in entity_json.get("spans", [])],
-            boxes=[Box.from_json(box_json=box_json)
-                   for box_json in entity_json.get('boxes', [])],
-            metadata=Metadata.from_json(entity_json.get('metadata', {}))
+            spans=[Span.from_json(span_json=span_json) for span_json in entity_json.get("spans", [])],
+            boxes=[Box.from_json(box_json=box_json) for box_json in entity_json.get("boxes", [])],
+            metadata=Metadata.from_json(entity_json.get("metadata", {})),
         )
 
     @property
     def start(self) -> Union[int, float]:
-        return (
-            min([span.start for span in self.spans])
-            if len(self.spans) > 0
-            else float("-inf")
-        )
+        return min([span.start for span in self.spans]) if len(self.spans) > 0 else float("-inf")
 
     @property
     def end(self) -> Union[int, float]:
-        return (
-            max([span.end for span in self.spans])
-            if len(self.spans) > 0
-            else float("inf")
-        )
+        return max([span.end for span in self.spans]) if len(self.spans) > 0 else float("inf")
 
     @property
     def symbols(self) -> List[str]:
         if self.doc is not None:
-            return [
-                self.doc.symbols[span.start: span.end] for span in self.spans
-            ]
+            return [self.doc.symbols[span.start : span.end] for span in self.spans]
         else:
             return []
 
@@ -96,4 +86,3 @@ class Entity(Annotation):
             return self.id < other.id
         else:
             return self.start < other.start
-
