7bed43a07430493c147374956f1491daeb82a7bb
kyleclo <kyleclo@uw.edu>
2023-08-03 20:50:09 -0700

    move files around

--- papermage/predictors/api_predictors/span_qa_predictor.py
+++ papermage/predictors/api_predictors/span_qa_predictor.py
@@ -1,18 +1,21 @@
 import json
 from typing import List
 
-from decontext.step.qa import TemplateRetrievalQAStep
 from decontext.data_types import (
-    PaperSnippet, PaperContext, QuestionAnswerEvidence, EvidenceParagraph
+    EvidenceParagraph,
+    PaperContext,
+    PaperSnippet,
+    QuestionAnswerEvidence,
 )
+from decontext.step.qa import TemplateRetrievalQAStep
 
 from papermage.predictors.base_predictor import BasePredictor
 from papermage.types import Annotation, Document
 
-class SpanQAPredictor(BasePredictor):
 
+class APISpanQAPredictor(BasePredictor):
     REQUIRED_BACKENDS = ["transformers", "torch", "decontext"]
-    
+
     def __init__(self, context_unit_name: str = "paragraph", span_name: str = "user_selected_span"):
         self.context_unit_name = context_unit_name
         self.span_name = span_name
@@ -22,18 +25,22 @@ class SpanQAPredictor(BasePredictor):
     @property
     def REQUIRED_DOCUMENT_FIELDS(self) -> List[str]:
         return [self.context_unit_name, self.span_name]
-    
+
     def preprocess(self, doc: Document) -> PaperSnippet:
-        paper_context = PaperContext.parse_raw(json.dumps({
-            "title": getattr(doc, "title") if "title" in doc.fields else "",
-            "abstract": getattr(doc, "abstract") if "abstract" in doc.fields else "",
-            "full_text": [{
-                "section_name": "",
-                "paragraphs":[
-                    entity.text for entity in getattr(doc, self.context_unit_name)
-                ]
-            }]
-        }))
+        paper_context = PaperContext.parse_raw(
+            json.dumps(
+                {
+                    "title": getattr(doc, "title") if "title" in doc.fields else "",
+                    "abstract": getattr(doc, "abstract") if "abstract" in doc.fields else "",
+                    "full_text": [
+                        {
+                            "section_name": "",
+                            "paragraphs": [entity.text for entity in getattr(doc, self.context_unit_name)],
+                        }
+                    ],
+                }
+            )
+        )
 
         paper_snippet = PaperSnippet(
             snippet=getattr(doc, self.span_name)[0].text,
@@ -42,13 +49,13 @@ class SpanQAPredictor(BasePredictor):
             paragraph_with_snippet=EvidenceParagraph(
                 section="",
                 paragraph=getattr(getattr(doc, self.span_name)[0], self.context_unit_name)[0].text,
-            )
+            ),
         )
 
         paper_snippet.add_question(getattr(doc, self.span_name)[0].metadata["question"])
 
         return paper_snippet
-    
+
     def _predict(self, doc: Document) -> List[Annotation]:
         # preprocess to format this for decontext
         paper_snippet = self.preprocess(doc)
@@ -58,12 +65,9 @@ class SpanQAPredictor(BasePredictor):
 
         # postprocess to format back into papermage doc format
         user_selected_span = getattr(doc, self.span_name)
-        
+
         user_selected_span[0].metadata["answer"] = paper_snippet.qae[0].answer
         user_selected_span[0].metadata["context_with_span"] = paper_snippet.paragraph_with_snippet.dict()
-        user_selected_span[0].metadata["retrieved_evidence"] = [
-            ev.dict()
-            for ev in paper_snippet.qae[0].evidence
-        ]
+        user_selected_span[0].metadata["retrieved_evidence"] = [ev.dict() for ev in paper_snippet.qae[0].evidence]
 
         return user_selected_span
--- tests/test_predictors/test_entity_classification_predictor.py
+++ tests/test_predictors/test_entity_classification_predictor.py
@@ -9,11 +9,10 @@ import pathlib
 import unittest
 
 from papermage.parsers import PDFPlumberParser
-from papermage.predictors import EntityClassificationPredictor
+from papermage.predictors import HFEntityClassificationPredictor
 from papermage.types import Document, Entity, Span
 
 TEST_SCIBERT_WEIGHTS = "allenai/scibert_scivocab_uncased"
-# TEST_BIB_PARSER_WEIGHTS = '/Users/kylel/ai2/mmda/stefans/'
 
 
 class TestEntityClassificationPredictor(unittest.TestCase):
@@ -28,12 +27,12 @@ class TestEntityClassificationPredictor(unittest.TestCase):
         ent1.id = 0
         ent2.id = 1
 
-        self.predictor = EntityClassificationPredictor.from_pretrained(
+        self.predictor = HFEntityClassificationPredictor.from_pretrained(
             model_name_or_path=TEST_SCIBERT_WEIGHTS, entity_name="tokens", context_name="pages"
         )
 
     def test_predict_pages_tokens(self):
-        predictor = EntityClassificationPredictor.from_pretrained(
+        predictor = HFEntityClassificationPredictor.from_pretrained(
             model_name_or_path=TEST_SCIBERT_WEIGHTS, entity_name="tokens", context_name="pages"
         )
         token_tags = predictor.predict(doc=self.doc)
@@ -64,7 +63,7 @@ class TestEntityClassificationPredictor(unittest.TestCase):
         self.predictor.context_name = "pages"
 
     def test_predict_pages_tokens_roberta(self):
-        predictor = EntityClassificationPredictor.from_pretrained(
+        predictor = HFEntityClassificationPredictor.from_pretrained(
             model_name_or_path="roberta-base",
             entity_name="tokens",
             context_name="pages",
