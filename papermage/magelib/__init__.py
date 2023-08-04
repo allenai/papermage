e5dabf35c3cfe02079be32549d49e2bfcd8143ff
kyleclo <kyleclo@uw.edu>
2023-08-04 01:46:07 -0700

    bugfix; imports got sorted

--- papermage/magelib/__init__.py
+++ papermage/magelib/__init__.py
@@ -6,24 +6,24 @@ Needs to be imported in this order.
 
 """
 
-from papermage.magelib.annotation import Annotation
-from papermage.magelib.box import Box
-from papermage.magelib.document import (
-    Document,
-    EntitiesFieldName,
-    ImagesFieldName,
-    MetadataFieldName,
-    PagesFieldName,
-    RelationsFieldName,
-    RowsFieldName,
-    SymbolsFieldName,
-    TokensFieldName,
-)
-from papermage.magelib.entity import Entity
+
 from papermage.magelib.image import Image
-from papermage.magelib.indexer import EntitySpanIndexer
-from papermage.magelib.metadata import Metadata
 from papermage.magelib.span import Span
+from papermage.magelib.box import Box
+from papermage.magelib.metadata import Metadata
+from papermage.magelib.annotation import Annotation
+from papermage.magelib.entity import Entity
+from papermage.magelib.indexer import EntitySpanIndexer
+from papermage.magelib.document import Document
+from papermage.magelib.document import (
+    MetadataFieldName, 
+    EntitiesFieldName, 
+    SymbolsFieldName,
+    RelationsFieldName, 
+    PagesFieldName, 
+    TokensFieldName, 
+    RowsFieldName
+)
 
 __all__ = [
     "Document",
