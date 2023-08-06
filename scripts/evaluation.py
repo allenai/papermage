1c15864057397c10a815afcf895220118574d294
Luca Soldaini <luca@soldaini.net>
2023-08-06 01:09:17 -0700

    no local folder

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -15,8 +15,8 @@ ap = ArgumentParser()
 ap.add_argument("vila", choices=["new", "old"])
 args = ap.parse_args()
 
-# dt = datasets.load_dataset('allenai/s2-vl', split='test')
-dt = datasets.load_dataset("json", data_dir="/net/nfs2.s2-research/lucas/s2-vl/data/", split="test")
+dt = datasets.load_dataset('allenai/s2-vl', split='test')
+# dt = datasets.load_dataset("json", data_dir="/net/nfs2.s2-research/lucas/s2-vl/data/", split="test")
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 if args.vila == "new":
