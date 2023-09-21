c9e5e79519cf299acda55b7b80a464df7f6d68b6
kyleclo <kyleclo@uw.edu>
2023-09-21 13:55:00 -0700

    making import relative

--- papermage/predictors/__init__.py
+++ papermage/predictors/__init__.py
@@ -1,12 +1,12 @@
-from papermage.predictors.base_predictors.base_predictor import BasePredictor
-from papermage.predictors.base_predictors.hf_predictors import HFBIOTaggerPredictor
-from papermage.predictors.block_predictors import LPEffDetPubLayNetBlockPredictor
-from papermage.predictors.formula_predictors import LPEffDetFormulaPredictor
-from papermage.predictors.sentence_predictors import PysbdSentencePredictor
-from papermage.predictors.span_qa_predictors import APISpanQAPredictor
-from papermage.predictors.token_predictors import HFWhitspaceTokenPredictor
-from papermage.predictors.vila_predictors import IVILATokenClassificationPredictor
-from papermage.predictors.word_predictors import SVMWordPredictor
+from .base_predictors.base_predictor import BasePredictor
+from .base_predictors.hf_predictors import HFBIOTaggerPredictor
+from .block_predictors import LPEffDetPubLayNetBlockPredictor
+from .formula_predictors import LPEffDetFormulaPredictor
+from .sentence_predictors import PysbdSentencePredictor
+from .span_qa_predictors import APISpanQAPredictor
+from .token_predictors import HFWhitspaceTokenPredictor
+from .vila_predictors import IVILATokenClassificationPredictor
+from .word_predictors import SVMWordPredictor
 
 __all__ = [
     "HFBIOTaggerPredictor",
